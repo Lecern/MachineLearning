96f2abc5588fc54d0cf1e5f93416ce89c1a17b4f
Jerry <gaozhiwei1993@gmail.com>
2019-10-04 11:37:05 +0900

    2.2

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
+import numpy as np
+
+randArr = np.random.randn(3, 2)
+
+randArr2 = np.random.randn(3, 2)
+
+print("random array 1:", randArr)
+
+print("random array 2:", randArr2)
+
+a = ("Apple", "Orange", "Banana")
+
+b = ("Tom", "Jack", "Alice")
+
+print(tuple(zip(a, b)))
+
 print("Hello world")
